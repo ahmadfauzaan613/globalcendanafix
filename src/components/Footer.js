87cad7ae6bdf0156078f2c4480a6e43483aef17b
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2026-02-08 13:02:46 +0700

    add  rel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
-import React from "react";
-import Logo from "../Assets/Logo.png";
-import call from "../Assets/call.png";
-import insta from "../Assets/instagram.png";
-import youtube from "../Assets/youtube.png";
-import linkedin from "../Assets/linkedin.png";
-import facebook from "../Assets/facebook.png";
+import Logo from '../Assets/Logo.png'
+import call from '../Assets/call.png'
+import facebook from '../Assets/facebook.png'
+import insta from '../Assets/instagram.png'
+import linkedin from '../Assets/linkedin.png'
+import youtube from '../Assets/youtube.png'
 
 function Footer() {
   return (
@@ -20,55 +19,64 @@ function Footer() {
         <div>
           <p className="text-[20px] text-[#262626] font-[400]">About Company</p>
           <p className="py-[10px] text-[16px] text-[#717070] font-light">
-            Duty the obligations of business will <br /> frequently occur that
-            pleasure have too <br /> repudiated annoyances endures <br />{" "}
-            accepted.
+            Duty the obligations of business will <br /> frequently occur that pleasure have too{' '}
+            <br /> repudiated annoyances endures <br /> accepted.
           </p>
         </div>
         <div className="phone:py-[20px] tablet:py-[20px] laptop:py-[20px]">
           <p className="text-[20px] text-[#262626] font-[400]">Contact Us</p>
           <div className="flex items-center space-x-2 pt-[10px]">
             <img src={call} alt="" className="w-[15px] h-[15px]" />
-            <p className=" text-[16px] text-[#717070] font-light">
-              085217713066
-            </p>
+            <p className=" text-[16px] text-[#717070] font-light">085217713066</p>
           </div>
           <div className="flex items-center space-x-2 py-[4px]">
             <span class="material-symbols-outlined text-[18px]">print</span>
             {/* <img src={call} alt="" className="w-[15px] h-[15px] " /> */}
-            <p className=" text-[16px] text-[#717070] font-light">
-              021 1231 3123
-            </p>
+            <p className=" text-[16px] text-[#717070] font-light">021 1231 3123</p>
           </div>
           <div className="flex items-center space-x-2 ">
             <span class="material-symbols-outlined text-[18px]">mail</span>
             {/* <img src={call} alt="" className="w-[15px] h-[15px]" /> */}
-            <p className=" text-[16px] text-[#717070] font-light">
-              mail@example.com
-            </p>
+            <p className=" text-[16px] text-[#717070] font-light">mail@example.com</p>
           </div>
         </div>
         <div className="phone:py-[20px] tablet:py-[20px] laptop:py-[20px]">
           <p className="text-[20px] text-[#262626] font-[400]">Social Media</p>
           <div className="pt-[10px] flex items-center space-x-2">
-            <a target="_blank"  href="https://instagram.com/globalcendanaekspor?igshid=Nzg3NjI1NGI=">
-            <img src={insta} className="w-[32px] h-full" alt="" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://instagram.com/globalcendanaekspor?igshid=Nzg3NjI1NGI="
+            >
+              <img src={insta} className="w-[32px] h-full" alt="" />
             </a>
-            <a target="_blank"  href="https://youtube.com/@globalcendanaekspor">
-            <img src={youtube} className="w-[32px] h-full" alt="" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://youtube.com/@globalcendanaekspor"
+            >
+              <img src={youtube} className="w-[32px] h-full" alt="" />
             </a>
-            <a target="_blank"  href="http://linkedin.com/in/pt-global-cendana-ekspor-90970825a">
-            <img src={linkedin} className="w-[32px] h-full" alt="" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://linkedin.com/in/pt-global-cendana-ekspor-90970825a"
+            >
+              <img src={linkedin} className="w-[32px] h-full" alt="" />
             </a>
-            <a target="_blank"  href="https://www.facebook.com/profile.php?id=100088289067795&mibextid=LQQJ4d">
-            <img src={facebook} className="w-[32px] h-full" alt="" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/profile.php?id=100088289067795&mibextid=LQQJ4d"
+            >
+              <img src={facebook} className="w-[32px] h-full" alt="" />
             </a>
           </div>
         </div>
         <div className="phone:py-[20px] tablet:py-[20px] laptop:py-[20px]">
           <p className="text-[20px] text-[#262626] font-[400]">Address</p>
           <p className="pt-[8px] text-[16px] text-[#717070] font-light">
-          JL.BUKIT PASIR NO.02 tenayan raya, <br /> kota pekanbaru, riau
+            JL.BUKIT PASIR NO.02 tenayan raya, <br /> kota pekanbaru, riau
           </p>
         </div>
       </div>
@@ -76,7 +84,7 @@ function Footer() {
         Copyright © 2022 PT. GLOBAL CENDANA EKSPOR
       </p>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
