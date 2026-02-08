450435379a75e7465438ebe8dedb6d7fe0ce3252
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2026-02-08 13:00:09 +0700

    delete import

--- src/components/Catalog/Template.js
+++ src/components/Catalog/Template.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import Navbar from '../Navbar'
 import Gambar1 from '../../Assets/coconut-milk-bg.png'
-import Footer from '../Footer'
+import Navbar from '../Navbar'
 
 function Template() {
   return (
@@ -14,16 +12,36 @@ function Template() {
         <p className="pt-[48px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <p>Artikel / 5 April 2022</p>
         <p className="pt-[20px] pb-[48px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pharetra ligula sit amet quam scelerisque, vel luctus quam congue. Fusce iaculis ut diam id egestas. Quisque ullamcorper efficitur ullamcorper. Vivamus in tellus ut risus pellentesque gravida maximus nec odio. Interdum et
-          malesuada fames ac ante ipsum primis in faucibus. Nam quis arcu sed metus egestas sagittis. In hac habitasse platea dictumst. Aliquam commodo dolor sit amet nisi luctus, nec lobortis tortor sollicitudin. Etiam congue purus mauris, eu dignissim lectus pretium vel. Pellentesque ligula velit,
-          mollis ac semper eget, tempor quis velit. Nullam nisl mauris, interdum in ornare non, maximus non dolor. Ut neque lorem, ullamcorper ut aliquet auctor, ultrices et dolor. Praesent a nibh accumsan, convallis lacus in, viverra leo. Integer gravida magna ac tortor dictum suscipit. Integer
-          metus leo, ullamcorper ac ante non, ultricies accumsan nisi. Nullam vitae vestibulum nulla. Morbi commodo justo id nisi pharetra faucibus. Ut gravida posuere lobortis. In ut eros quam. Vestibulum sollicitudin mauris ut lectus cursus consectetur. Fusce vel pulvinar ex. Etiam auctor tempor
-          aliquam. Morbi at vulputate turpis. Maecenas dolor magna, suscipit quis facilisis nec, ultrices et quam. Sed dictum mauris eget luctus interdum. Suspendisse a mi accumsan, accumsan mi ac, varius ligula. Maecenas quis purus consectetur, blandit sem id, tincidunt eros. Mauris tristique
-          feugiat lacus, in sollicitudin nisl consectetur euismod. Quisque porttitor, tellus non pulvinar tristique, nunc nisi vulputate tellus, efficitur ultrices metus mauris non risus. Sed a ornare dolor. Cras dictum, erat quis viverra ullamcorper, felis nunc lobortis magna, sed aliquam tortor
-          magna nec metus. Quisque nec diam auctor enim tincidunt dapibus nec in massa. Quisque sed felis ac mi laoreet placerat ut vel sapien. Fusce laoreet egestas mauris, nec blandit massa pellentesque et. Suspendisse in consectetur est. Phasellus pulvinar a elit a mattis. Integer mattis posuere
-          tellus in commodo. Curabitur hendrerit mi egestas varius eleifend. Vestibulum viverra placerat odio, ac placerat velit porta ut. Sed cursus mollis leo et ullamcorper. Cras neque elit, viverra sed feugiat a, fermentum sed augue. Vestibulum pellentesque, est nec placerat rhoncus, lacus metus
-          congue orci, semper interdum libero ligula tristique leo. Pellentesque id rhoncus metus. Vestibulum quis urna quis metus facilisis luctus et id nunc. Donec ac diam hendrerit, rhoncus dolor sit amet, tincidunt mauris. Donec at dui sit amet ante auctor auctor eu vitae mauris. Morbi fermentum
-          tellus at elementum viverra.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pharetra ligula sit
+          amet quam scelerisque, vel luctus quam congue. Fusce iaculis ut diam id egestas. Quisque
+          ullamcorper efficitur ullamcorper. Vivamus in tellus ut risus pellentesque gravida maximus
+          nec odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam quis arcu sed
+          metus egestas sagittis. In hac habitasse platea dictumst. Aliquam commodo dolor sit amet
+          nisi luctus, nec lobortis tortor sollicitudin. Etiam congue purus mauris, eu dignissim
+          lectus pretium vel. Pellentesque ligula velit, mollis ac semper eget, tempor quis velit.
+          Nullam nisl mauris, interdum in ornare non, maximus non dolor. Ut neque lorem, ullamcorper
+          ut aliquet auctor, ultrices et dolor. Praesent a nibh accumsan, convallis lacus in,
+          viverra leo. Integer gravida magna ac tortor dictum suscipit. Integer metus leo,
+          ullamcorper ac ante non, ultricies accumsan nisi. Nullam vitae vestibulum nulla. Morbi
+          commodo justo id nisi pharetra faucibus. Ut gravida posuere lobortis. In ut eros quam.
+          Vestibulum sollicitudin mauris ut lectus cursus consectetur. Fusce vel pulvinar ex. Etiam
+          auctor tempor aliquam. Morbi at vulputate turpis. Maecenas dolor magna, suscipit quis
+          facilisis nec, ultrices et quam. Sed dictum mauris eget luctus interdum. Suspendisse a mi
+          accumsan, accumsan mi ac, varius ligula. Maecenas quis purus consectetur, blandit sem id,
+          tincidunt eros. Mauris tristique feugiat lacus, in sollicitudin nisl consectetur euismod.
+          Quisque porttitor, tellus non pulvinar tristique, nunc nisi vulputate tellus, efficitur
+          ultrices metus mauris non risus. Sed a ornare dolor. Cras dictum, erat quis viverra
+          ullamcorper, felis nunc lobortis magna, sed aliquam tortor magna nec metus. Quisque nec
+          diam auctor enim tincidunt dapibus nec in massa. Quisque sed felis ac mi laoreet placerat
+          ut vel sapien. Fusce laoreet egestas mauris, nec blandit massa pellentesque et.
+          Suspendisse in consectetur est. Phasellus pulvinar a elit a mattis. Integer mattis posuere
+          tellus in commodo. Curabitur hendrerit mi egestas varius eleifend. Vestibulum viverra
+          placerat odio, ac placerat velit porta ut. Sed cursus mollis leo et ullamcorper. Cras
+          neque elit, viverra sed feugiat a, fermentum sed augue. Vestibulum pellentesque, est nec
+          placerat rhoncus, lacus metus congue orci, semper interdum libero ligula tristique leo.
+          Pellentesque id rhoncus metus. Vestibulum quis urna quis metus facilisis luctus et id
+          nunc. Donec ac diam hendrerit, rhoncus dolor sit amet, tincidunt mauris. Donec at dui sit
+          amet ante auctor auctor eu vitae mauris. Morbi fermentum tellus at elementum viverra.
         </p>
         <div className="border-y py-[20px]">
           <p>Related Posts</p>
@@ -34,7 +52,9 @@ function Template() {
           </div>
         </div>
 
-        <p className="text-center text-[14px] text-[#717070] pb-[12px] pt-[16px] font-light">Copyright © 2022 PT. GLOBAL CENDANA EKSPOR</p>
+        <p className="text-center text-[14px] text-[#717070] pb-[12px] pt-[16px] font-light">
+          Copyright © 2022 PT. GLOBAL CENDANA EKSPOR
+        </p>
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import Article from '../components/Article'
+import Whatsapp from '../Assets/whatsapp.png'
 import Content from '../components/Content'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
@@ -9,7 +8,6 @@ import Number from '../components/Number'
 import Photo from '../components/Photo'
 import Product from '../components/Product'
 import Team from '../components/Team'
-import Whatsapp from '../Assets/whatsapp.png'
 
 function Home() {
   return (
@@ -24,9 +22,17 @@ function Home() {
       <Team />
       <Location />
       <Footer />
-      
-      <a  target="_blank"  href="https://api.whatsapp.com/send?phone=6285217713066"  className="absolute bottom-4 desktop3:right-6 desktop2:right-6 desktop:right-4 phone:right-4 tablet:right-4 laptop:right-4 cursor-pointer">
-        <img src={Whatsapp} alt="" className="desktop3:w-[2vw] desktop2:w-[4vw] desktop:w-[4vw] phone:w-[9vw] tablet:w-[5vw] laptop:w-[4vw] h-full" />
+
+      <a
+        target="_blank"
+        href="https://api.whatsapp.com/send?phone=6285217713066"
+        className="absolute bottom-4 desktop3:right-6 desktop2:right-6 desktop:right-4 phone:right-4 tablet:right-4 laptop:right-4 cursor-pointer"
+      >
+        <img
+          src={Whatsapp}
+          alt=""
+          className="desktop3:w-[2vw] desktop2:w-[4vw] desktop:w-[4vw] phone:w-[9vw] tablet:w-[5vw] laptop:w-[4vw] h-full"
+        />
       </a>
     </div>
   )
